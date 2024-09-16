95aa62226ed5f6c4271f42ebe1b0ae50d97219ad
Avuta Shashidhar <161814579+avutashashi291@users.noreply.github.com>
2024-09-16 09:03:50 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -2,7 +2,7 @@ module "s3" {
   source      = "./s3"
   for_each    = var.bucket
   bukcet_name = each.value.bucket_name
-  depends_on = [ module.vpc ]
+  depends_on = [ module.VPC ]
 }
 
 module "VPC" {
